fba0dd1e6d9536589cb8a31dc5698b8977fa5fab
jsamudio <Jonathan_Samudio1@baylor.edu>
2026-09-16 08:29:28 -0500

    Add validation for MadJax

--- generation/closure.py
+++ generation/closure.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import uproot
 import hist
 import awkward as ak
@@ -8,30 +9,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mplhep as hep
 import warnings
-plt.style.use(hep.style.CMS)
 
+plt.style.use(hep.style.CMS)
 NanoAODSchema.warn_missing_crossrefs = False
+
+
+def decode_WCnames(wc_names_raw):
+    WCnames_out = []
+    WCname = ''
+    for val in wc_names_raw:
+        fourchar = int(val).to_bytes(4, 'big').decode()
+        if '-' in fourchar:
+            WCname += fourchar.split('-')[-1]
+        else:
+            if len(WCname):
+                WCnames_out.append(WCname)
+            WCname = fourchar.lstrip('\x00')
+    WCnames_out.append(WCname)
+    return ["SM"] + WCnames_out
+
+
+def get_wc_weight(events, wc_names, name='sm', value=0.0):
+    sm_weight = events.EFTfitCoefficients[:, 0]
+
+    if name.lower() == 'sm':
+        return sm_weight
+
+    wc_idx = wc_names.index(name)
+    index1 = events.EFTfitCoefficientIndex1[0]
+    index2 = events.EFTfitCoefficientIndex2[0]
+
+    matching1 = np.where((index1 == wc_idx) & (index2 == 0))[0]
+    matching2 = np.where((index1 == wc_idx) & (index2 == wc_idx))[0]
+
+    wc_lin = ak.flatten(events.EFTfitCoefficients[:, matching1] * value)
+    wc_sq = ak.flatten(events.EFTfitCoefficients[:, matching2] * value**2)
+
+    return sm_weight + wc_lin + wc_sq
+
 
 if __name__ == '__main__':
 
-    import argparse
-
-    argParser = argparse.ArgumentParser(description = "Argument parser")
+    argParser = argparse.ArgumentParser(description="Argument parser")
+    argParser.add_argument('--fixed', action='store',
+                           default='root://eosuser.cern.ch//eos/user/j/jsamudio/madjaxTut/nanogen_ttbar_ctg1.root',
+                           help="Fixed sample file")
+    argParser.add_argument('--reweighted', action='store',
+                           default='root://eosuser.cern.ch//eos/user/j/jsamudio/madjaxTut/nanogen_ttbar_madjax.root',
+                           help="Reweighted sample file")
     argParser.add_argument('--output', action='store', default='./closure.pdf', help="Output file")
+    argParser.add_argument('--wc-name', action='store', default='ctgre', help="Wilson Coefficient name (e.g. ctgre)")
+    argParser.add_argument('--wc-value', action='store', type=float, default=1.0, help="Wilson Coefficient value")
     args = argParser.parse_args()
 
     events_fixed = NanoEventsFactory.from_root(
-        'root://cmseos.fnal.gov//store/user/dspitzba/EFT/nanogen_fixed_123.root',
+        args.fixed,
         schemaclass=NanoAODSchema,
     ).events()
-    nevents_fixed = len(events_fixed.LHEWeight)
-
 
     events_reweighted = NanoEventsFactory.from_root(
-        'root://cmseos.fnal.gov//store/user/dspitzba/EFT/nanogen_123.root',
+        args.reweighted,
         schemaclass=NanoAODSchema,
     ).events()
-    nevents_reweighted = len(events_reweighted.LHEWeight)
+
+    # Extract dynamic MadJax weights
+    wc_names = decode_WCnames(events_reweighted.WCnames[0])
+    mj_weight = get_wc_weight(events_reweighted, wc_names, name=args.wc_name, value=args.wc_value)
 
     ht_axis = hist.axis.Regular(25, 100, 1500, name="ht", label="HT", underflow=True, overflow=True)
 
@@ -42,13 +85,15 @@ if __name__ == '__main__':
 
     h_reweighted.fill(
         ht=ak.sum(events_reweighted.GenJet.pt, axis=1),
-        weight=getattr(events_reweighted.LHEWeight, 'EFTrwgt10_ctGRe_2.0_ctGIm_0.0_ctWRe_0.0_ctWIm_0.0_ctBRe_0.0_ctBIm_0.0_cHtbRe_0.0_cHtbIm_0.0_cHt_0.0'),
+        weight=mj_weight,
     )
 
     h_fixed = hist.Hist(
         ht_axis,
         storage=hist.storage.Weight(),
     )
+
+    # Kept exactly as in original non-MadJax script (unweighted fill relying on density=True later)
     h_fixed.fill(
         ht=ak.sum(events_fixed.GenJet.pt, axis=1),
     )
@@ -57,18 +102,17 @@ if __name__ == '__main__':
 
     h_fixed.plot1d(
         ax=ax,
-        label=r'$C_{tG}=2 (fixed)$',
+        label=f'{args.wc_name}={args.wc_value} (fixed)',
         density=True,
     )
     h_reweighted.plot1d(
         ax=ax,
-        label=r'$C_{tG}=2 (reweighted)$',
+        label=f'{args.wc_name}={args.wc_value} (reweighted)',
         density=True,
     )
 
     ax.set_ylabel(r'a.u.')
-    ax.set_xlabel(r'$H_{T} (GeV)$')
-
+    ax.set_xlabel(r'\(H_{T} (GeV)\)')
     ax.set_yscale("log")
 
     plt.legend()
